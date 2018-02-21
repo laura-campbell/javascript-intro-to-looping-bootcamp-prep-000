950e49147c163544d5f2b74f7e7a1c6195b371c0
Laura Campbell <campbell.laura.elizabeth@gmail.com>
2018-02-21 02:46:58 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ var i;
 function forLoop (arr) {
   for (i=0; i <25; i++) {
   if (i===1) {arr.push('I am 1 strange loop.')}
-  else {arr.push('I am ${i} strange loops.')}
+  else {arr.push('I am ' + i + ' strange loops.')}
   }
   return arr;
 }
